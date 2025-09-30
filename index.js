fe3e48a35d8b9dcb443830317a2569e5443251dd
Devang <devangdadhich22@gmail.com>
2025-09-30 19:53:03 +0530

    Add GitHub user data endpoint and update .gitignore formatting

--- index.js
+++ index.js
@@ -5,6 +5,46 @@ const app = express();
 
 const port = 4000;
 
+const gitdata = {
+  login: "Shivang-dadhich",
+  id: 184916609,
+  node_id: "U_kgDOCwWagQ",
+  avatar_url: "https://avatars.githubusercontent.com/u/184916609?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/Shivang-dadhich",
+  html_url: "https://github.com/Shivang-dadhich",
+  followers_url: "https://api.github.com/users/Shivang-dadhich/followers",
+  following_url:
+    "https://api.github.com/users/Shivang-dadhich/following{/other_user}",
+  gists_url: "https://api.github.com/users/Shivang-dadhich/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/Shivang-dadhich/starred{/owner}{/repo}",
+  subscriptions_url:
+    "https://api.github.com/users/Shivang-dadhich/subscriptions",
+  organizations_url: "https://api.github.com/users/Shivang-dadhich/orgs",
+  repos_url: "https://api.github.com/users/Shivang-dadhich/repos",
+  events_url: "https://api.github.com/users/Shivang-dadhich/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/Shivang-dadhich/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+  name: "SHIVANG DADHICH",
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: null,
+  public_repos: 6,
+  public_gists: 0,
+  followers: 2,
+  following: 11,
+  created_at: "2024-10-14T08:08:47Z",
+  updated_at: "2025-09-18T05:21:00Z",
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -19,6 +59,10 @@ app.get("/login", (req, res) => {
 
 app.get("/youtube", (req, res) => {
   res.send("<h2>chai or code</h2>");
+});
+
+app.get("/github", (req, res) => {
+  res.json(gitdata);
 });
 
 app.listen(process.env.PORT, () => {
